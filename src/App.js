dca1ed0a09afccb82b21e8842501b68b54c9c632
rogones <93624147+rogones@users.noreply.github.com>
2023-01-03 20:59:23 +0100

    Anita

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//aplicación actualizada con Hello
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from V2_Anita</h1>
       </header>
     </div>
   );
 }
-
-export default App;
+ export default App;
